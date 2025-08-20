2b89dcf22c9314bb074c6e9442fe1046a51cbc51
bako110 <bakorobert2000@gmail.com>
2025-08-20 06:53:53 +0200

    commit

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-const SECRET_KEY = process.env.JWT_SECRET;
+const SECRET_KEY = process.env.JWT_SECRET || 'apcdd_secret_key';
 
 
 // Login
